af0786aa8b8c2345e533a3ebf18edb7bb35aba2c
Joy Biswas <joybiswas389@gmail.com>
2024-12-27 21:02:03 +0600

    finished module-3 and added module-4

--- module-3-managing-data-and-working-with-volumes/nodeapp/commands.sh
+++ module-3-managing-data-and-working-with-volumes/nodeapp/commands.sh
@@ -10,6 +10,34 @@ docker stop feedback-app
 docker run -d -p 3000:80 --rm --name feedback-app nodeapp:volumes #still fails
 docker volume ls
 docker run -d -p 3000:80 -v feedback:/app/feedback --rm --name feedback-app nodeapp:volumes #named volume
-docker run -d -p 3000:80 -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp":/app --rm --name feedback-app nodeapp:volumes #bind mounts
+docker run -d -p 3000:80 -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp:/app" --rm --name feedback-app nodeapp:volumes #bind mounts
 
-docker run -d -p 3000:80 -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp":/app -v /app/node_modules --rm --name feedback-app nodeapp:volumes
+docker run -d -p 3000:80 -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp:/app" -v /app/node_modules --rm --name feedback-app nodeapp:volumes
+
+# Making the bind mount Read-Only, but feedback and temp folders should had read-write access
+docker run -d -p 3000:80 -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp:/app:ro" -v /app/temp -v /app/node_modules --rm --name feedback-app nodeapp:volumes
+
+
+# Check all volumes
+docker volume ls
+
+# Create a new volume
+docker volume create feedback-files
+
+# Inspect a volume
+docker volumes inspect feedback-files
+
+# Remove a volume
+docker volume rm feedback-files
+
+docker build -t nodeapp:env . 
+
+# Using env to set PORT
+docker run -d -p 3000:8000 --env PORT=8000 --name feedback-app -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp:/app:ro" -v /app/temp -v /app/node_modules --rm nodeapp:env 
+
+
+# Using .env file to set PORT
+docker run -d -p 3000:8000 --env-file .env --name feedback-app -v feedback:/app/feedback -v "C:\Udemy\Docker\module-3-managing-data-and-working-with-volumes\nodeapp:/app:ro" -v /app/temp -v /app/node_modules --rm nodeapp:env
+
+# Using build arguments
+docker build -t nodeapp:dev --build-arg DEFAULT_PORT=3000 .
