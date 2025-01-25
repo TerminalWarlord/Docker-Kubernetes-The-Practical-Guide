6d072e540a18a640be03cb1b23fcc38fb028e175
terminalwarlord <joybiswas389@gmail.com>
2025-01-25 22:38:24 +0600

    pod internal communication implemented

--- module-14-kubernetes-networking/kub-network-01-starting-setup/commands.sh
+++ module-14-kubernetes-networking/kub-network-01-starting-setup/commands.sh
@@ -6,3 +6,17 @@ kubectl get deployments # shows all deployments
 
 kubectl get services  # shows all services
 
+docker build -t terminalwarlord/kub-demo-user ./users-api
+
+docker push terminalwarlord/kub-demo-user
+
+kubectl apply -f kubernetes/user-deployment.yaml
+
+kubectl get pods
+
+kubectl apply -f kubernetes/user-service.yaml
+
+kubectl get services
+
+
+docker build -t terminalwarlord/kub-demo-auth .
