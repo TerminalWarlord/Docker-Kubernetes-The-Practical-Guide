784fbdac266622375edf2c859b90392c4d04005e
terminalwarlord <joybiswas389@gmail.com>
2025-01-26 00:55:45 +0600

    testing the frontend locally

--- module-14-kubernetes-networking/kub-network-01-starting-setup/commands.sh
+++ module-14-kubernetes-networking/kub-network-01-starting-setup/commands.sh
@@ -20,3 +20,20 @@ kubectl get services
 
 
 docker build -t terminalwarlord/kub-demo-auth .
+
+
+kubectl apply -f=auth-service.yaml,auth-deployment.yaml  
+
+ubectl apply -f=users-service.yaml,users-deployment.yaml
+
+
+kubectl exec users-595c9d8db4-nvhmj -- printenv # shows the env variables
+
+
+
+kubectl get namespaces
+
+kubectl logs tasks-776cbcdd7-fhlms
+
+
+docker run -p 80:80 --rm -d terminalwarlord/kub-demo-frontend 
